28901ffd126ccb0ec02ec11498d9863078bf6418
victoriak94 <vkamila6@gmail.com>
2019-03-16 15:59:13 -0400

    Write create method in controller

--- app/controllers/shortened_urls_controller.rb
+++ app/controllers/shortened_urls_controller.rb
@@ -8,5 +8,21 @@ class ShortenedUrlsController < ApplicationController
         redirect_to @url.sanitize_url
     end
 
+    def create
+        @url = ShortenedUrl.new
+        @url.original_url = params[:original_url]
+        @url.sanitize
+        if @url.new_url?
+            if @url.save
+                redirect_to shortened_path(@url.short_url)
+            else
+                flash[:error] = "Check the error below:"
+                render 'index'
+            end
+        else
+            flash[:notice] = "A Short link for this URL already exists"
+            redirect_to shortened_path(@url.find_duplicate.short_url)
+        end
+    end
 
 end
