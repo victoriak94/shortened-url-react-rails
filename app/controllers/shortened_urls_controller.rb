2602b61c53cfe0365855297068633bb4ea6c7dcc
victoriak94 <vkamila6@gmail.com>
2019-03-16 16:32:24 -0400

    Write getch_original_url and find_url and url_params methods

--- app/controllers/shortened_urls_controller.rb
+++ app/controllers/shortened_urls_controller.rb
@@ -32,4 +32,17 @@ class ShortenedUrlsController < ApplicationController
         @short_url = host + '/' + @url.short_url
     end
 
+    def fetch_original_url
+        fetch_url = ShortenedUrl.find_by_short_url(params[:short_url])
+        redirect_to fetch_url.sanitize_url
+    end
+
+    private
+    def find_url
+        @url = ShortenedUrl.find_by_short_url(params[:short_url])
+    end
+
+    def url_params
+        params.require(:url).permit(:original_url)
+    end
 end
