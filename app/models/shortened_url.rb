01922c0212b9f9bfb5fcdfe041957df5d9cd1a77
victoriak94 <vkamila6@gmail.com>
2019-03-16 15:45:32 -0400

    Create unique_id_length, write validations for ShortenedUrl model and before_create statements. Write generate_short_url function

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -1,2 +1,19 @@
 class ShortenedUrl < ApplicationRecord
+    UNIQUE_ID_LENGTH = 7
+    validates :original_url, presence: true, on: :create
+    vlidates_format_of :original_url,
+        with: /\A(?:(?:http|https):\/\/)?([-a-zA-Z0-9.]{2,256}\.[a-z]{2,4})\b(?:\/[-a-zA-Z0-9@,!:%_\+.~#?&\/\/=]*)?\z/
+    before_create :generate_short_url
+    before_create :sanitize
+
+    # Unique URL function
+    def generate_short_url
+        url = ([*('a'..'z'),*('0'..'9')]).sample(UNIQUE_ID_LENGTH).join
+        old_url = ShortenedUrl.where(short_url: url).last
+        if old_url.present?
+            self.generate_short_url
+        else
+            self.short_url = url
+        end
+    end
 end
